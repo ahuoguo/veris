aea3a750f9b28b1ed6fe9d7c25b06650c38eb3fd
ahuoguo <ahuoguo@gmail.com>
2025-12-17 15:09:40 +0800

    less hacky spec fn

--- ub/rand_primitives.rs
+++ ub/rand_primitives.rs
@@ -38,9 +38,8 @@ pub fn rand_u64(bound: u64,
   (val, Tracked::assume_new())
 }
 
-// TODO: temporary hack for parsing error... (see zulip)
-pub open spec fn flip_e2(a:bool) -> (spec_fn(int) -> int) {
-  |a:int| if a == 1 { 0 } else { 1 }
+pub open spec fn flip_e2(x: int) -> int {
+  if x == 1 { 0 } else { 1 }
 }
 
 proof fn ec_contradict(tracked e: ErrorCreditResource)
@@ -58,32 +57,35 @@ pub fn flip(Tracked(e1): Tracked<ErrorCreditResource>) -> (ret: u64)
   requires
     (ErrorCreditCarrier::Value{car: 0.5real}) == e1.view()
 {
-  assert(average(2u64, flip_e2(true)) == 0.5real) by {
-    assert(Seq::new(2 as nat, |i:int| i) =~= seq![0, 1]);
-    // TODO: is there a more automatic way to unfold this fold_left?
+  assert(Seq::new(2 as nat, |i:int| i) =~= seq![0, 1]);
+  // TODO: is there a more automatic way to unfold this fold_left?
+  // TODO: unfortunabte hack for `spec_fn`, see zulip: 
+  // https://verus-lang.zulipchat.com/#narrow/channel/399078-help/topic/.E2.9C.94.20Using.20.60spec.20fn.60.20as.20.60spec_fn.60/near/564030019
+  assert(average(2u64, (|y:int| flip_e2(y))) == 0.5real) by {
+    let f = (|y:int| flip_e2(y)); // LOOK AT ME: you can't just use flip_e2 here...
     calc! {
       (==)
-      seq![0, 1].fold_left(0int, |acc: int, x| acc + flip_e2(true)(x)); {
+      seq![0, 1].fold_left(0int, |acc: int, x| acc + f(x)); {
         assert(seq![0int, 1].drop_last() =~= seq![0]);
       }
-      seq![0].fold_left(0int, |acc: int, x| acc + flip_e2(true)(x)) + flip_e2(true)(1);
+      seq![0].fold_left(0int, |acc: int, x| acc + f(x)) + f(1);
       {
         assert(seq![0int].drop_last() =~= seq![]);
       }
-      (seq![].fold_left(0int, |acc: int, x| acc + flip_e2(true)(x)) + flip_e2(true)(0)) + flip_e2(true)(1);
+      (seq![].fold_left(0int, |acc: int, x| acc + f(x)) + f(0)) + f(1);
     }
-    assert(seq![0, 1].fold_left(0int, |acc: int, x| acc + flip_e2(true)(x)) == 1int);
+    assert(seq![0, 1].fold_left(0int, |acc: int, x| acc +  f(x)) == 1int);
     assert(1real / 2real == 0.5real);
   };
   // Need to wrap with ghost becuase argument must be exec mode
   // https://verus-lang.github.io/verus/guide/reference-var-modes.html#using-tracked-and-ghost-variables-from-an-exec-function
-  let (val, Tracked(e2)) = rand_u64(2u64, Tracked(e1), Ghost(flip_e2(true)));
+  let (val, Tracked(e2)) = rand_u64(2u64, Tracked(e1), Ghost(|x:int| flip_e2(x)));
 
   // TODO: some how you can't put `proof {...}` in `assert by`
   // and `assert by {...}` is not considered as a proof block
   proof {
     if(val != 1) {
-      assert(flip_e2(true)(val as int) == 1);
+      // owns ↯(ℰ(1)) -> contradiction
       ec_contradict(e2);
     }
   }
