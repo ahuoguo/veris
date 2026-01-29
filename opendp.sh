e1a1b33810f6a3af685e9c394c0fcd7fb6341898
ahuoguo <ahuoguo@gmail.com>
2026-01-29 00:06:06 -0500

    correct syntax

--- opendp.sh
+++ opendp.sh
@@ -4,12 +4,9 @@ set -e
 
 BASEDIR=$(realpath $(dirname "$0"))
 
-# build the rlib file for libc crate, which verus-mimalloc takes as a dependency
-
 cd $BASEDIR
 mkdir -p build
 
-# TOOD: should I be careful about the version?
 cd random
 # cargo clean --release
 cargo +1.92.0 build --release
